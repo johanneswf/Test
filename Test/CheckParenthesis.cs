e854ee24d7bfaf9be2c41605bc873393a578146b
johanneswf <johanneswf@gmail.com>
2022-06-01 11:23:48 +0200

    Testing array enumeration

---
+++ Test/CheckParenthesis.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class CheckParenthesis
+    {
+        bool Basic(string input)
+        {
+            char[,] balancedPairs = new char[,] { { '(', ')' },
+                                                  { '{', '}' },
+                                                  { '[', ']' } };
+
+            int maxRows = balancedPairs.GetLength(0);
+
+            Stack<char> matches = new();
+
+            foreach (char c in input)
+            {
+                for (int currentRow = 0; currentRow < maxRows; currentRow++)
+                {
+                    if (c == balancedPairs[currentRow, 1])
+                    {
+                        if ((matches.Count == 0) || (matches.Pop() != balancedPairs[currentRow, 0])) return false;
+                        break;
+                    }
+                    if (c == balancedPairs[currentRow, 0])
+                    {
+                        matches.Push(c);
+                        break;
+                    }
+                }
+            }
+
+            if (matches.Count != 0) return false;
+
+            return true;
+        }
+
+        //bool Dict(string input)
+        //{
+        //    var balancedPairs = new Dictionary<char, char>
+        //            {
+        //                { ')', '(' },
+        //                { '}', '{' },
+        //                { ']', '[' }
+        //            };
+
+        //    var inputCharacterMatches = new Stack<char>();
+
+        //    foreach (char inputCharacter in input)
+        //    {
+        //        if ((balancedPairs.ContainsKey(inputCharacter)) &&
+        //            (inputCharacterMatches.Count == 0 || inputCharacterMatches.Pop() != balancedPairs.GetValueOrDefault(inputCharacter))) return false;
+        //        if (balancedPairs.ContainsValue(inputCharacter)) inputCharacterMatches.Push(inputCharacter);
+        //    }
+
+        //    if (inputCharacterMatches.Count != 0) return false;
+
+        //    return true;
+        //}
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,11 +1,51 @@
 ﻿using System.Collections;
 
-namespace Test
+
+SeedGarage();
+
+
+
+static void SeedGarage()
 {
-    internal class Program
+    var garage = new Garage<Vehicle>(5) { new Vehicle("stoc"), new Vehicle("mog") };
+    garage.Add(new Vehicle("roro"));
+
+    foreach (var g in garage)
     {
-        static void Main(string[] args)
-        {
-        }
+        if (g != null) Console.WriteLine(g.Name);
     }
+}
+
+class Vehicle
+{
+    public string Name;
+    public Vehicle(string name)
+    {
+        Name = name;
+    }
+}
+
+class Garage<T> : IEnumerable<T>
+{
+    private readonly T[] _garage;
+    private readonly int _size;
+
+    public Garage(int size)
+    {
+        _size = size;
+        _garage = new T[_size];
+    }
+
+    public bool Add(T v)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (_garage[i] == null) { _garage[i] = v; return true; }
+        }
+        return false;
+    }
+
+    public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)_garage).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => _garage.GetEnumerator();
 }
